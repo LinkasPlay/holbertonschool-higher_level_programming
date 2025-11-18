ff64277454009208bf844ecd7465f05d0f0e5665
Lukas Borsarini <linkas.play7@gmail.com>
2025-11-18 14:06:39 +0100

    Print alphabet excluding 'e' and 'q'

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(1):
-    print("abcdfghijklmnoprstuvwxyz")
 
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('e') and i != ord('q'):
+        print("{:c}".format(i), end="")
