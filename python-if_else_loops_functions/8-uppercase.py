a7aef6d8e83e23b259ba0c9cc7785c73d12f9b6d
Lukas <12610@holbertonstudents.com>
2025-11-19 14:56:38 +0000

    en mismo casa

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
