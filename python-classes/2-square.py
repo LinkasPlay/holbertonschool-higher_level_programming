2b5e1f192faffda662faee6deb280ab814caf082
Lukas Borsarini <linkas.play7@gmail.com>
2025-11-21 11:29:47 +0100

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class with a private size attribute.
+"""
+
+
 class Square:
-    """Represents a squre."""
-    population = 0
+    """
+    This class represents a square and enforces size validation.
+    """
 
-    def __init__(self, name):
-        """Initializes the data."""
-        self.size = size
-    def die(self):
-        """I am dying."""
-      
-    def say_hi(self):
-        """Greeting by the robot.
-        
-    @classmethod
-    def how_many(cls):
-        """Prints the current population."""
-        print("We have {:d} robots.".format(cls.population))
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with size validation.
 
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
