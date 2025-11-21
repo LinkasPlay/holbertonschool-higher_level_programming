ad3ccf89bc2c41ea63bd62c6885e7435b017a4cd
Lukas Borsarini <linkas.play7@gmail.com>
2025-11-21 11:19:39 +0100

    Refine docstring for Square class module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-This module defines an empty Square class that can be expanded later
-to represent geometric squares with different properties.
+This module defines an empty Square class for geometry-related operations.
 """
 
 
 class Square:
     """
-    This class represents a square and will be expanded in future tasks
-    to include attributes and behaviors related to square geometry.
+    This class defines a square and will be expanded in future tasks.
     """
     pass
