cbde8f3e1e207bcfe2928d11914c859c6ed01905
Lukas Borsarini <linkas.play7@gmail.com>
2025-11-21 11:02:49 +0100

    Add shebang to 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a squre."""
+    population = 0
+
+    def __init__(self, name):
+        """Initializes the data."""
+        self.size = size
+    def die(self):
+        """I am dying."""
+      
+    def say_hi(self):
+        """Greeting by the robot.
+        
+    @classmethod
+    def how_many(cls):
+        """Prints the current population."""
+        print("We have {:d} robots.".format(cls.population))
+
