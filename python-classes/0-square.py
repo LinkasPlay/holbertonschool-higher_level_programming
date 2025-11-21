a1a315babb7e2e6c63f6663bbdcce6c6e274425d
Lukas Borsarini <linkas.play7@gmail.com>
2025-11-21 11:45:27 +0100

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,3 @@
-#!/usr/bin/python3
-"""
-This module defines a Square class.
-"""
-
-
-class Square:
-    """
-    Represents a square.
-    """
+#!/usr/bin/python3 
+class Square: """un caré""" 
     pass # An empty block
